2f4697bb91d4e5d7aab5347a35f5aa6efe7c8d97
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-03-06 18:57:34 -0300

    spawningPipes

--- tappyplane/scenes/pipes/pipes.gd
+++ tappyplane/scenes/pipes/pipes.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
-const SPEED: float = 100.0
+class_name Pipes
+
+const SPEED: float = 120.0
 
 func _process(delta: float) -> void:
 	position.x -= SPEED * delta
+	
+func check_off_screen() -> void:
+	if position.x < -500.0:
+		queue_free()
 
 func _on_screen_exited() -> void:
 	queue_free()
