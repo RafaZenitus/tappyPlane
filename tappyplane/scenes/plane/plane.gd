79b73b9ec4eeb4aef07d0bd8abc4a040cde6ff12
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-03-06 18:13:37 -0300

    animationsCreated

--- tappyplane/scenes/plane/plane.gd
+++ tappyplane/scenes/plane/plane.gd
@@ -1,15 +1,31 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 300.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+const GRAVITY: float = 1000.0
+const POWER: float = -400.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
-
-
+	pass     
 
 func _physics_process(delta: float) -> void:
 	
-	velocity.y += GRAVITY #* delta
+	velocity.y += GRAVITY * delta
+	
+	fly()
 	
 	move_and_slide() #move_and_slide is already multiplied by delta 
+	if is_on_floor() == true:
+		die()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("jump") == true:
+		velocity.y = POWER
+		animation_player.play("RESET")
+		animation_player.play("power")
+		
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
