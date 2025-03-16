490df7a0403496e38ff6c829fb0249436f3fc50e
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-03-16 17:26:44 -0300

    restaringGameWorking

--- tappyplane/scenes/game/game.gd
+++ tappyplane/scenes/game/game.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 const PIPES = preload("res://scenes/pipes/pipes.tscn")
+var MAIN = load("res://scenes/main/main.tscn")
 
 @onready var spawn_u: Marker2D = $SpawnU
 @onready var spawn_l: Marker2D = $SpawnL
 @onready var spawn_timer: Timer = $SpawnTimer
 @onready var pipes_holder: Node = $PipesHolder
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit") and event.is_echo() == false:
+		#print("_unhandled_input: ", event)
+		get_tree().change_scene_to_packed(MAIN)
 
 func _ready() -> void:
 	spawn_pipes()
@@ -27,4 +33,5 @@ func _on_spawn_timer_timeout() -> void:
 
 
 func _on_plane_die() -> void:
-	get_tree().paused = true
+	#get_tree().paused = true
+	pass
--- tappyplane/scenes/main/main.gd
+++ tappyplane/scenes/main/main.gd
@@ -1,11 +1,16 @@
 extends Control
 
+const GAME = preload("res://scenes/game/game.tscn")
 
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("jump") and event.is_echo() == false:
+		#print("_inpnut: ", event)
+		#get_viewport().set_input_as_handled()
 
-func _input(event: InputEvent) -> void:
-	print(event)
-
-
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and event.is_echo() == false:
+		#print("_unhandled_input: ", event)
+		get_tree().change_scene_to_packed(GAME)
 
 func _ready() -> void:
 	pass # Replace with function body.
